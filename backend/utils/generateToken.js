f8bda33240eb1d2ace7ed769dbce3b2cc1a66d70
AbdirahmanMoahemd <62654786+AbdirahmanMoahemd@users.noreply.github.com>
2021-06-30 16:55:30 +0300

    Prepare for deployment

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
-
+const JWT_SECRET = 'abc123'
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, JWT_SECRET, {
         expiresIn: '30d'
     })
 }
