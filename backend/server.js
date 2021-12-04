095003b5d5d9720436f21982f5be5f21912adfd5
AbdirahmanMoahemd <kaah6978@gmail.com>
2021-12-05 01:00:54 +0300

    make it better

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ else {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT =  5000
+const PORT =  process.env.PORT || 5000
 
-app.listen(process.env.PORT || 5000, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
