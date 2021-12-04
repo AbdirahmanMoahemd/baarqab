4d058bc900da235c7b16914ba14cfddc536971b3
AbdirahmanMoahemd <kaah6978@gmail.com>
2021-12-05 01:42:34 +0300

    alx

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ import products  from './data/products.js'
 dotenv.config()
 connectDB()  
 const app = express()
-const NODE_ENV = 'production'
+
 const PAYPAL_CLIENT_ID = 'ATe2evqLhzMwvul-WeBEi3kKXeVFeVXIUBtorMlmeWnmIPH8elu1q-Kd2YTgDrDPBy_Fjwvm_PMTY9ah'
-if (NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
@@ -51,7 +51,7 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if (NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req, res) => 
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
@@ -69,5 +69,5 @@ app.use(errorHandler)
 
 const PORT =  process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
